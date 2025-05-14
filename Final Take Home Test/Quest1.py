9065421f01c67129b17edb3d0f8a06d1eb29d6e9
David Long <dllong@cpp.edu>
2025-05-14 14:23:58 -0700

    TakeHomeTest quest 1 work

--- Final Take Home Test/Quest1.py
+++ Final Take Home Test/Quest1.py
@@ -2,28 +2,25 @@ import requests
 import xmltodict
 import json
 import random
+import threading
+from queue import Queue, Empty
 from datetime import datetime, timedelta
 
+
 def listofDates():
-    start_date = datetime.strptime("2025-05-04", "%Y-%m-%d")
+    start_date = datetime.strptime("2011-05-04", "%Y-%m-%d")
     end_date = datetime.today()
     change = end_date - start_date
 
-    global dates
     dates = []
     for i in range(change.days):
         date = (start_date + (timedelta(i)))
         dates.append(date.strftime("%Y-%m-%d"))
-    
+    return dates
 
+def download(date):
 
-rates = ["EUR", "GBP", "USD", "DZD", "AUD", "BWP", "BND", "CAD", "CLP", "CNY", "COP", "CZK", "DKK", "HUF", "ISK", "INR", "IDR", "ILS", "KZT", "KRW", "KWD", "LYD", "MYR", "MUR", "NPR", "NZD", "NOK", "OMR", "PKR", "PLN", "QAR", "RUB", "SAR", "SGD", "ZAR", "LKR", "SEK", "CHF", "THB", "TTD"]
-ratesForBase = [r for r in rates if r != "USD" and r != "EUR" and r != "GBP"]
-
-
-for date in dates:
     # URL of the XML data
-    base = random.choice(ratesForBase)
     url = f"https://www.floatrates.com/historical-exchange-rates.html?operation=rates&pb_id=1775&page=historical&currency_date={date}&base_currency_code={base}&format_type=xml"
     print(url)
     # Fetch the XML data
@@ -40,5 +37,42 @@ for date in dates:
     print(json_data)
 
     # Optionally, write the JSON data to a file
-    with open(f"{date}_exchange_rates_{base}.json", "w") as json_file:
+    with open(f"Final Take Home Test/Rates/{date}_exchange_rates_{base}.json", "w") as json_file:
         json_file.write(json_data)
+
+def worker(work_queue):
+    while not work_queue.empty():
+        try:
+            date = work_queue.get(block=False)
+        except Empty:
+            break
+        else:
+            download(date)
+            work_queue.task_done()
+
+def thread_pool(dates, size=15):
+    work_queue = Queue()
+
+    for date in dates:
+        work_queue.put(date)
+
+    threads = []
+    for _ in range(size):
+        thread = threading.Thread(target=download, args=(work_queue,))
+        threads.append(thread)
+
+    for thread in threads:
+        thread.start()
+
+    work_queue.join()
+
+    for thread in threads:
+        thread.join()
+
+rates = ["EUR", "GBP", "USD", "DZD", "AUD", "BWP", "BND", "CAD", "CLP", "CNY", "COP", "CZK", "DKK", "HUF", "ISK", "INR", "IDR", "ILS", "KZT", "KRW", "KWD", "LYD", "MYR", "MUR", "NPR", "NZD", "NOK", "OMR", "PKR", "PLN", "QAR", "RUB", "SAR", "SGD", "ZAR", "LKR", "SEK", "CHF", "THB", "TTD"]
+ratesForBase = [r for r in rates if r != "USD" and r != "EUR" and r != "GBP"]
+
+dates = listofDates()
+base = random.choice(ratesForBase)
+
+thread_pool(dates)
